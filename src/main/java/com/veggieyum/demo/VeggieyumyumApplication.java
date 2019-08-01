61a5b8440a3d82145bc6f7dd8e34bf418b85d131
AngelicaSantos <angelica.santos2113@gmail.com>
2019-08-01 17:08:50 -0400

    initial commit

---
+++ src/main/java/com/veggieyum/demo/VeggieyumyumApplication.java
@@ -0,0 +1,13 @@
+package com.veggieyum.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeggieyumyumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeggieyumyumApplication.class, args);
+    }
+
+}
